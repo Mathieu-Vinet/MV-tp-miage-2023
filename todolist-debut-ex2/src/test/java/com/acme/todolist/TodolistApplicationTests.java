353b16f4a3486396b2b97b2a78b4b69836bf5cd8
Mathieu <math2400@gmail.com>
2023-03-17 12:14:28 +0100

    Exos 3 - TU

--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
@@ -1,13 +1,23 @@
 package com.acme.todolist;
 
+import com.acme.todolist.domain.TodoItem;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.time.Instant;
 
-@SpringBootTest
+@SpringBootTest(classes = TodoItem.class)
 class TodolistApplicationTests {
+	private TodoItem todoItem;
 
 	@Test
 	void contextLoads() {
+
+		todoItem = new TodoItem("test", Instant.now(), "Je suis pas late :)");
+		Assertions.assertEquals("Je suis pas late :)",todoItem.finalContent());
+
+		todoItem = new TodoItem("test", Instant.parse("2020-02-27T10:31:43Z"), "Je suis late :)");
+		Assertions.assertEquals("[LATE!] Je suis late :)",todoItem.finalContent());
 	}
 
 }
